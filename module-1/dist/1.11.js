981e5d324398bca36dd7a39008c824f7f70cdb75
mahfuj <mahfujahmad424@gmail.com>
2024-05-01 23:05:51 +0600

    final commi t

--- module-1/dist/1.11.js
+++ module-1/dist/1.11.js
@@ -1,9 +1,14 @@
 "use strict";
 //  ternary operator || optional chaining || nullish coalescing operator
-const age = 15;
+const age = 18;
 if (age >= 18) {
-    console.log("you are a adult person");
+    //   console.log("you are a adult person");
 }
 else {
-    console.log("you are not adult person");
+    //   console.log("you are not adult person");
 }
+const isAdult = age >= 18 ? 'adult' : "not adult";
+// console.log(isAdult);
+const checkAdult = null;
+const result = checkAdult !== null && checkAdult !== void 0 ? checkAdult : 'adult';
+console.log(result);
---
+++ module-1/dist/1.12.js
@@ -0,0 +1,11 @@
+"use strict";
+// nullable type 
+const searchName = (value) => {
+    if (value) {
+        console.log('searching');
+    }
+    else {
+        console.log('no found any name');
+    }
+};
+searchName('mahfuj');
